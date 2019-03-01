2583c300cadc12999d837ca289a1ec14b8c38c83
IsmaelGar <igarcia91@g.ucla.edu>
2019-02-28 16:44:48 -0800

    Update MAX5825BAUP+.c

--- MAX5825BAUP+.c
+++ MAX5825BAUP+.c
@@ -34,10 +34,19 @@ https://datasheets.maximintegrated.com/en/ds/MAX5823-MAX5825.pdf
 
 
 /**
-
+DAC_pwr(uint16_t,uint16_t,int);
+	uint16_t address    	 : DAC i2c address
+	uint16_t channel    	 : DAC channel 7-0             
+	int 	 mode            : PWR Mode
+                               00: Normal Mode
+                               01: Power down with 1kOhm termination to GND
+                               10: Power down with 100kOhm termination to GND
+                               11: Power down with Hi-Z
 **/
-void DAC_pwr(){
-	
+void DAC_pwr(uint16_t address, uint16_t channel,int mode){
+ wiringPiI2CWriteReg16(address,0x40,((channel&0xFF)<<8) | ((mode&0x03) << 6));
+ 
+
 }
 
 
@@ -47,23 +56,23 @@ Dac_config(uint16_t,uint16_t,int,int,int,int);
 	uint16_t address    	 : DAC i2c address
 	uint16_t channel    	 : DAC channel 7-0
 	int 	 watchdogConfig  : WDOG config
-								00:DIS
-								01:GATE
-								10:CLR
-								11:HOLD
-	int 	 gateEnb		 : Gate Configuration
-								0:Enables  software gating function 
-								1:Disables software gating function 
-	int 	 ldacEnb		 : Load Configuration
-								0:Enables   DAC latch(LDAC and Load functions)
-								1:Disables  DAC latch(LDAC and Load functions)
-	int 	 clearEnb		 : Clear Configuration
-								0:Clear input and command functions impact the DAC
-								1:Clear input and command functions does not impact the DAC
+                            	   00:DIS
+                                   01:GATE
+                                   10:CLR
+                                   11:HOLD
+	int 	 gateEnb         : Gate Configuration
+                                   0:Enables  software gating function 
+                                   1:Disables software gating function 
+	int 	 ldacEnb         : Load Configuration
+                                   0:Enables   DAC latch(LDAC and Load functions)
+                                   1:Disables  DAC latch(LDAC and Load functions)
+	int 	 clearEnb        : Clear Configuration
+                                   0:Clear input and command functions impact the DAC
+                                   1:Clear input and command functions does not impact the DAC
 **/
 
 uint16_t DAC_config(uint16_t address, uint16_t channel,int watchdogConfig,int gateEnb,int ldacEnb,int clearEnb){
-wiringPiI2CWriteReg16(address,0x50,((channel&0xFF)<<8) | ((watchdogConfig&0xFF) << 6) | ((gateEnb & 0x01) <<5) | ((ldacEnb&0x01) << 4)| ((clearEnb& 0x01) <<3));
+ wiringPiI2CWriteReg16(address,0x50,((channel&0xFF)<<8) | ((watchdogConfig&0xFF) << 6) | ((gateEnb & 0x01) <<5) | ((ldacEnb&0x01) << 4)| ((clearEnb& 0x01) <<3));
 }
 
 
